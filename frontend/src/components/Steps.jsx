f21037ae85163e485ac81d7e851db9d97370ceb4
Abraham <abrahamalfonzo11gmail.com>
2024-08-31 20:43:01 -0400

    pasos update

--- frontend/src/components/Steps.jsx
+++ frontend/src/components/Steps.jsx
@@ -1,13 +1,14 @@
 export function Steps(){
     return (
         <section className="pasos_padre">
-            <div className="cont_pasos paso1">
-            <img src="https://github.com/abram001X/convertMoonJs/blob/main/frontend/src/img/Opera%20Instant%C3%A1nea_2024-08-30_084321_rr2---sn-pouxgj5cax-jjnl.googlevideo.com.jpg?raw=true"  className="img_pasos"  />
-            <p className="p_pasos">Después de dar click en descargar video, <br /> la pagina lo reedirije a una página con el vídeo...Y tiene que ir a la parte inferior derecha y dar click</p>
+            <h2>Como descargar un video(mp4) después de convertirlo</h2>
+            <div className="cont_pasos ">
+            <div className="img_pasos paso1"></div>
+            <p className="p_pasos">PASO 1 : <br />  Después de dar click en descargar video, <br /> la pagina lo reedirije a una página con el vídeo...Y tiene que ir a la parte inferior derecha y dar click</p>
             </div>
-            <div className="cont_pasos paso2">
-                <img className="img_pasos" src="https://github.com/abram001X/convertMoonJs/blob/main/frontend/src/img/Opera%20Instant%C3%A1nea_2024-08-30_090007_rr2---sn-pouxgj5cax-jjnl.googlevideo.com.jpg?raw=true"  />
-                <p className="p_pasos">Luego dar click en descargar y empezará la descarga :)</p>
+            <div className="cont_pasos">
+                <div className="img_pasos paso2"></div>
+                <p className="p_pasos">PASO 2 : <br />  Luego dar click en descargar y empezará la descarga :)</p>
             </div>
         </section>
     )
